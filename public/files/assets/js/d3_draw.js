cad6a940322d0ab6c4310df4992853de9aabf53f
minyoung <enfto@naver.com>
2019-11-27 17:00:54 +0900

    1127

--- public/files/assets/js/d3_draw.js
+++ public/files/assets/js/d3_draw.js
@@ -953,6 +953,7 @@ diagram.selectAll(".dot")
 //그룹 라인차트
 function grouplinechart(data){
     d3.select('.svgclass').remove();
+
     var margin =  {top: 20, right: 20, bottom: 100, left: 80};
     var marginOverview = {top: 30, right: 10, bottom: 20, left: 40};
     var selectorHeight = 40;
@@ -966,48 +967,130 @@ function grouplinechart(data){
     var x_value = Object.keys(data[0])[1]
     var y_value = Object.keys(data[0])[2]
     var isScrollDisplayed = barWidth * data.length > width;
-
+	
+	 var keys = Object.keys(data[0]).slice(2);
+	 var z = d3.scaleOrdinal()
+    .range(["#FE9365", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    
+    if(x_value == 'CAR_ID'){
+        if(subclass == 'Accel' || subclass == 'Decel' || subclass == 'QuickStart' || subclass == 'SuddenStop'){
+            y_value = Object.keys(data[0])[7]
+			keys = Object.keys(data[0]).slice(7);
+        } else if(subclass == 'FuelEfficiency'){
+            y_value = Object.keys(data[0])[4]
+            keys = Object.keys(data[0]).slice(4);
+        }
+    }
     console.log(isScrollDisplayed)
-    var keys = Object.keys(data[0]).slice(2);
-    var xscale = d3.scalePoint()
-    .domain(data.slice(0,numBars).map(function (d) {return d[x_value];}))
-    .rangeRound([0, width])
-    .padding(0.5);
 
-    var yscale = d3.scaleLinear()
-    .rangeRound([height, 0])
-    .domain([0, d3.max(data, function (d) { return d3.max(keys, function (key) { return parseInt(d[key]); }); })]).nice();
+// The number of datapoints
 
-    var z = d3.scaleOrdinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+// 5. X scale will use the index of our data
+var xscale = d3.scalePoint()
+.domain(data.slice(0,numBars).map(function (d) {return d[x_value];}))
+.rangeRound([0, width])
+.padding(0.5);
 
-    var svg = d3.select(".svg-container").append("svg")
+// 6. Y scale will use the randomly generate number 
+var yscale = d3.scaleLinear()
+    .domain([0, d3.max(data, function (d) { return parseInt(d[y_value]); })]) // input 
+    .range([height, 0]); // output 
+    
+    var xAxis  = d3.axisBottom(xscale);
+    var yAxis  = d3.axisLeft(yscale).ticks(8);
+	
+	//background line
+	function make_x_gridlines() {		
+		return d3.axisBottom(xscale)
+        .ticks(5)
+	}
+
+	// gridlines in y axis function
+	function make_y_gridlines() {		
+		return d3.axisLeft(yscale)
+		.ticks(5)
+    }
+
+
+//여기부터 삽입
+
+z.domain(d3.keys(data[0]).filter(function(key){
+    console.log("key",key)
+    if(key!=="CAR_ID" && key!=="time") return key;
+  }))
+
+  var namevalue = z.domain().map(function(name){
+    return {
+      name:name,
+      values:data.slice(0, numBars).map(function(d){
+            return {
+                CAR_ID:d[x_value],
+                value:+d[name]
+              };
+      })
+    };
+  });
+console.log("namevalue",namevalue);
+
+// 7. d3's line generator
+var line = d3.line()
+    .x(function(d) { return xscale(d[x_value]); }) // set the x values for the line generator
+    .y(function(d) { return yscale(d.value); }) // set the y values for the line generator 
+    .curve(d3.curveMonotoneX) // apply smoothing to the line
+
+    var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<strong>"+d[x_value]+":</strong> <span style='color:red'>" + d[y_value] + "</span>";
+    })
+
+	var subline = d3.line()
+    .x(function(d) {return xOverview(d[x_value]);})
+    .y(function(d) {return height + heightOverview + yOverview(d[y_value])})
+	.curve(d3.curveMonotoneX) // apply smoothing to the line
+
+	// 1. Add the SVG to the page and employ #2
+	var svg = d3.select(".svg-container").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom + selectorHeight)
     .attr("class", "svgclass")
+    .call(tip)
     .call(responsivefy);
 
-    //background line
-function make_x_gridlines() {		
-    return d3.axisBottom(xscale)
-    .ticks(5)
-}
+    
+	var diagram = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// gridlines in y axis function
-function make_y_gridlines() {		
-    return d3.axisLeft(yscale)
-    .ticks(5)
-}
+    diagram.selectAll('.line-group')
+    .data(namevalue).enter()
+    .append('g')
+    .attr('class', 'line-group')  
+    .append('path')
+    .attr('class', 'line')  
+    .attr("d", function(d){return line(d.values)}) // 11. Calls the line generator 
+    .style("stroke",function(d){
+        return z(d.name)
+      });
 
 
-    var diagram = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .attr("class", "gcontainer");
+      diagram.selectAll('.circle-group')
+      .data(namevalue).enter()
+      .append('g')
+      .attr('class', 'circle-group')  
+      .append('circle')
+      .attr('class', 'dot')  
+      .attr("class", "dot") // Assign a class for styling
+      .attr("cx", function(d, i) { return xscale(d.values[x_value]) })
+      .attr("cy", function(d) { return yscale(d.values.value) })
+      .attr("r", 5)
+        .style("stroke",function(d){
+          return z(d.name)
+        });
 
 
-    var xAxis  = d3.axisBottom(xscale);
-    var yAxis  = d3.axisLeft(yscale).ticks(8);
-
-    diagram.append("g")
+	// 3. Call the x axis in a group tag
+	diagram.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .style("font","12px Verdana")
@@ -1018,32 +1101,7 @@ function make_y_gridlines() {
     .attr("class", "y axis")
     .style("font","12px Verdana")
     .call(yAxis);
-var i = 0;
-
-var line = d3.line().x(function(d) {
-     return xscale(d[x_value]); })
-     .y(function(d) { return yscale(d[keys[i]]); })
-     .curve(d3.curveMonotoneX);
-
-var lines = svg.selectAll(".gcontainer")
-    .data(data.slice(0, numBars))
-    .enter().append("g")
-    .attr("class", "lines")
-
-// 9. Append the path, bind the data, and call the line generator 
-lines.append("path")
-    .attr("class", "line")
-    .attr("d", line(data.slice(0, numBars)))
-    
-// 12. Appends a circle for each datapoint 
-lines.append("circle") // Uses the enter().append() method
-    .attr("class", "dot") // Assign a class for styling
-    .attr("cx", function(d, i) { return xscale(d[x_value]) })
-    .attr("cy", function(d) { return yscale(d[keys[i]]) })
-    .attr("r", 5)
-    .attr("fill", function (d) { return z(d.key); });
- 
-
+	
 	//background line
 	diagram.append("g")			
       .attr("class", "grid")
@@ -1060,34 +1118,22 @@ lines.append("circle") // Uses the enter().append() method
           .tickFormat("")
     )
 	
-           //레전드 
-            diagram.append("text")
-          .attr("class", "label")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 0 - margin.left - 3)
-          .attr("x",0 - (height / 2))
-          .attr("dy", "1em")
-          .style("text-anchor", "middle")
-          .text(kyaxis()); 
-        
-	
-	var x_labels = diagram.append("text")    
-	  .attr("class", "label")		
-      .attr("transform",
-            "translate(" + (width/2) + " ," + 
-                           (height + margin.top +30) + ")")
-      .style("text-anchor", "middle")
-      .text(kxaxis());
-    if (isScrollDisplayed){
-		x_labels.attr("transform",
-            "translate(" + (width/2) + " ," + 
-                           (height + margin.bottom) + ")")
-	}
-
-    var legend = diagram.append("g")
+		           //레전드 
+                    diagram.append("text")
+                  .attr("class", "label")
+                  .attr("transform", "rotate(-90)")
+                  .attr("y", 0 - margin.left - 3)
+                  .attr("x",0 - (height / 2))
+                  .attr("dy", "1em")
+                  .style("text-anchor", "middle")
+                  .text(kyaxis()); 
+                
+	//범례
+	    var legend = diagram.append("g")
     .attr("font-family", "sans-serif")
     .attr("font-size", 10)
     .attr("text-anchor", "end")
+	//.attr("style", "outline: thin solid red;")   //This will do the job
     .selectAll("g")
     .data(keys.slice().reverse())
     .enter().append("g")
@@ -1104,44 +1150,47 @@ lines.append("circle") // Uses the enter().append() method
     .attr("y", 9.5)
     .attr("dy", "0.32em")
     .text(function (d) { return klegend(d); });
-    
+	
+	var x_labels = diagram.append("text")  
+	  .attr("class", "label")			
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top +30) + ")")
+      .style("text-anchor", "middle")
+      .text(kxaxis());
+    if (isScrollDisplayed){
+		x_labels.attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.bottom) + ")")
+	}
+
+
+
     if (isScrollDisplayed)
     {
-
-        var xOverview = d3.scaleBand()
-        .rangeRound([0, width])
-        .paddingInner(0.1);
-    
-        var xOverview1 = d3.scaleBand()
-        .padding(0.05);
-
-         xOverview = d3.scaleBand()
-        .domain(data.map(function (d) { return d[x_value]; }))
-        .rangeRound([0, width], .2)
-        .padding(0.2);     
-         xOverview1.domain(keys).rangeRound([0, xOverview.bandwidth()]);
-    
-      
+      var xOverview = d3.scalePoint()
+      .domain(data.map(function (d) { return d[x_value]; }))
+      .rangeRound([0, width])
+      .padding(0.5);      
                       
       yOverview = d3.scaleLinear().range([heightOverview, 0]);
       yOverview.domain(yscale.domain());
     
-      var bar = diagram.selectAll(".gcontainer")
-    .data(data)
-    .enter().append("g")
-    .attr("class","subbars")
-    .attr("transform", function (d) { return "translate(" + xOverview(d[x_value]) + ",0)"; })
-    
-    bar.selectAll("rect")
-    .data(function (d) { return keys.map(function (key) { return { key: key, value: d[key] }; }); })
-    .enter().append("rect")
-    .classed('subBar', true)
-    .attr("x", function (d) { return xOverview1(d.key);})
-    .attr("y", function (d) { return height + heightOverview + yOverview(d.value); })
-    .attr("width", xOverview1.bandwidth())
-    .attr("height", function (d) { return heightOverview - yOverview(d.value); })
-    .attr("fill", function (d) { return z(d.key); });
       
+      diagram.append("path")
+      .datum(data)
+          .classed('subBar', true)
+          .attr(
+            "height", function(d) {
+                return heightOverview - yOverview(d[y_value]);
+            })
+        .attr(
+            "width", function(d) {
+                return xOverview.bandwidth()
+            })
+          .attr("d", subline);
+          
+              
       var displayed = d3.scaleQuantize()
                   .domain([0, width])
                   .range(d3.range(data.length));
@@ -1149,6 +1198,7 @@ lines.append("circle") // Uses the enter().append() method
       diagram.append("rect")
                   .attr("transform", "translate(0, " + (height + margin.top) + ")")
                   .attr("class", "mover")
+                  .attr("background-color","black")
                   .attr("x", 0)
                   .attr("y", 0)
                   .attr("height", selectorHeight)
@@ -1171,35 +1221,33 @@ lines.append("circle") // Uses the enter().append() method
         nf = displayed(nx);
     
         if ( f === nf ) return;
-     //   var keys = Object.keys(data[0]).slice(1);
-
         new_data = data.slice(nf, nf + numBars);
 
-        var keys = Object.keys(new_data[0]).slice(2);
-
-        xscale.domain(new_data.slice(0,numBars).map(function (d) { return d[x_value]; }));
-        xscale1.domain(keys).rangeRound([0, xscale.bandwidth()]);
+        xscale.domain(new_data.map(function (d) { return d[x_value]; }));
         diagram.select(".x.axis").call(xAxis);
-        
-        var rect = svg.selectAll(".bars")
-        .data(new_data);
-        
-        rect.enter().append("g")
-        .attr("class","bars")
-        .attr("transform", function (d) { return "translate(" + xscale(d[x_value]) + ",0)"; });
-      
-        var rect2 = rect.selectAll("rect")
-        .data(function (d) { return keys.map(function (key) { return { key: key, value: d[key] }; }); });
 
-        rect2.enter().append("rect")
-        .attr("width", xscale1.bandwidth());
+        dots = diagram.selectAll(".dot")
+        dots.data(new_data)
+        .enter().append("circle") // Uses the enter().append() method
+        .merge(dots)
+          .attr("class", "dot") // Assign a class for styling
+          .attr("cx", function(d, i) { return xscale(d[x_value]) })
+          .attr("cy", function(d) { return yscale(d[y_value]) })
+          .attr("r", 5)
+          dots.exit().remove();
 
-        rect2.attr("x", function (d) { return xscale1(d.key);})
-        .attr("y", function (d) { return yscale(d.value); })
-        .attr("height", function (d) { return height - yscale(d.value); })
-        .attr("fill", function (d) { return z(d.key); });
-        
-        rect2.exit().remove();
+        rects = diagram.selectAll(".line")
+          rects.datum(new_data)
+          .attr("class", "line") // Assign a class for styling 
+          .attr("d", line); // 11. Calls the line generator 
+
+
+    
+    // 	  rects.attr("transform", function(d) { return "translate(" + xscale(d.label) + ",0)"; })
+    
+
+    
+
     };
 
 }
